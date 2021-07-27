9b59e95894d5c6a9901c2e85de1ceca87adb0302
Robert J. Rouquette <robert.rouquette@focusvq.com>
2021-07-27 11:01:28 -0500

    add msp430 demo code

--- msp430/main.c
+++ msp430/main.c
@@ -1,6 +1,22 @@
-
+#include <msp430.h>
 
 int main() {
+    // TODO remove demo code
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                            // to activate previously configured port settings
+    P1DIR |= 0x01;                          // Set P1.0 to output direction
+
+    for(;;) {
+        volatile unsigned int i;            // volatile to prevent optimization
+
+        P1OUT ^= 0x01;                      // Toggle P1.0 using exclusive-OR
+
+        i = 10000;                          // SW Delay
+        do i--;
+        while(i != 0);
+    }
+
     // TODO bootsrap MCU
     return 0;
 }
