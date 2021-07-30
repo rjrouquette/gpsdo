3497f354cf6e620134a18bd91ffd0825ad45c857
Robert J. Rouquette <robert.rouquette@focusvq.com>
2021-07-30 14:18:36 -0500

    more verbose comments

--- msp430/tcxo.c
+++ msp430/tcxo.c
@@ -65,28 +65,41 @@ static void storeBin() {
  * @return the DCXO digital frequency offset
 **/
 int32_t TCXO_getCompensation(int16_t tempC) {
+    int64_t rem;
+    int32_t m, b;
+
+    // load temperature bin
     uint8_t binId = getBinId(tempC);
     loadBin(binId);
 
-    int32_t A = getCell(0); // 0.32
-    int32_t B = getCell(1); // 16.16
-    int32_t C = getCell(2); // 24.8
-    int32_t D = getCell(3); // 32.0
+    // output -2^31 if there is no data
+    if(currBin.norm == 0) return 0x80000000l;
 
-    int32_t Z = A - mult32s(B, B);
-    // require stable data
-    if(Z <= 0) return D;
-    // require at least 64 samples
+    // load average offset for the bin
+    int32_t D = getCell(3); // 32.0
+    // require at least 64 samples before performing OLS fit
     if(currBin.norm < 0x200000u) return D;
 
-    int32_t m, b;
-    int64_t rem;
+    // load XX cells
+    int32_t A = getCell(0); // 0.32
+    int32_t B = getCell(1); // 16.16
+    // compute determinant
+    int32_t Z = A - mult32s(B, B);
+    // must satisfy E(x^2) > E(x)^2
+    if(Z <= 0) return D;
+
+    // load XY cell
+    int32_t C = getCell(2); // 24.8
+    // compute m
     rem = (mult64s(C, 256) - mult64s(B, D)) << 24;
     divS(Z, &rem, &m);
+    // compute b
     rem = mult64s(A, D) - (mult64s(B, C) << 8);
     divS(Z, &rem, &b);
 
+    // get fractional part of temperature
     int16_t x = getFracTemp(tempC);
+    // apply coefficients
     return (mult64s(m, x) >> 16u) + b;
 }
 
@@ -96,15 +109,18 @@ int32_t TCXO_getCompensation(int16_t tempC) {
  * @param offset - the DCXO digital frequency offset in 32.0 fixed point format
 **/
 void TCXO_updateCompensation(int16_t tempC, int32_t offset) {
-    // compute binId
+    // load temperature bin
     uint8_t binId = getBinId(tempC);
     loadBin(binId);
 
+    // get fractional part of temperature
     int16_t x = getFracTemp(tempC);
+    // update accumulators
     increment();
     accumulate(0, mult32s(x, x) << 16u);
     accumulate(1, x << 8u);
     accumulate(2, mult64s(offset, x));
     accumulate(3, offset);
+    // store results
     storeBin();
 }
