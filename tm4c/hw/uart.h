b6f2d11f3a840fb68505f1a28cd1449530ee439d
Robert J. Rouquette <robert.rouquette@focusvq.com>
2022-05-23 14:26:31 -0500

    complete UART header

--- tm4c/hw/uart.h
+++ tm4c/hw/uart.h
@@ -8,6 +8,14 @@
 #include "register.h"
 #include "gpio.h"
 
+enum UART_FIFO_INT_LVL {
+    UART_FIFO_INT_LVL_1_8 = 0x0,
+    UART_FIFO_INT_LVL_1_4 = 0x1,
+    UART_FIFO_INT_LVL_1_2 = 0x2,
+    UART_FIFO_INT_LVL_3_4 = 0x3,
+    UART_FIFO_INT_LVL_7_8 = 0x4
+};
+
 PAGE_MAP (UART_MAP, {
     // offset 0x000
     // UART Data Register
@@ -67,24 +75,156 @@ PAGE_MAP (UART_MAP, {
     // offset 0x02C
     // UART Line Control
     REGMAP_32 (, {
-            unsigned BRK: 1;
-            unsigned PEN: 1;
-            unsigned EPS: 1;
-            unsigned STP2: 1;
-            unsigned FEN: 1;
-            unsigned WLEN: 2;
-            unsigned SPS: 1;
-    }) UARTLCRH;
+        unsigned BRK: 1;
+        unsigned PEN: 1;
+        unsigned EPS: 1;
+        unsigned STP2: 1;
+        unsigned FEN: 1;
+        unsigned WLEN: 2;
+        unsigned SPS: 1;
+    }) LCRH;
 
-    uint32_t IM;        // UART Interrupt Mask
-    uint32_t RIS;       // UART Raw Interrupt Status
-    uint32_t MIS;       // UART Masked Interrupt Status
-    uint32_t ICR;       // UART Interrupt Clear
-    uint32_t DMACTL;    // UART DMA Control
-    char _reserved_00[0xF98];
+    // offset 0x030
+    // UART Control
+    REGMAP_32(, {
+        unsigned UARTEN: 1;
+        unsigned SIREN: 1;
+        unsigned SIRLP: 1;
+        unsigned SMART: 1;
+        unsigned EOT: 1;
+        unsigned HSE: 1;
+        unsigned : 1;
+        unsigned LBE: 1;
+        unsigned TXE: 1;
+        unsigned RXE: 1;
+        unsigned DTR: 1;
+        unsigned RTS: 1;
+        unsigned : 2;
+        unsigned RTSEN: 1;
+        unsigned CTSEN: 1;
+    }) CTL;
+
+    // offset 0x034
+    // UART Interrupt FIFO Level Select
+    REGMAP_32(, {
+        unsigned TXIFLSEL: 3;
+        unsigned RXIFLSEL: 3;
+    }) IFLS;
+
+    // offset 0x038
+    // UART Interrupt Mask
+    REGMAP_32(, {
+        unsigned RI: 1;
+        unsigned CTS: 1;
+        unsigned DCD: 1;
+        unsigned DSR: 1;
+        unsigned RX: 1;
+        unsigned TX: 1;
+        unsigned RT: 1;
+        unsigned FE: 1;
+        unsigned PE: 1;
+        unsigned BE: 1;
+        unsigned OE: 1;
+        unsigned EOT: 1;
+        unsigned BIT9: 1;
+        unsigned : 3;
+        unsigned DMARX: 1;
+        unsigned DMATX: 1;
+    }) IM;
+
+    // offset 0x03C
+    // UART Raw Interrupt Status
+    REGMAP_32(, {
+        unsigned RI: 1;
+        unsigned CTS: 1;
+        unsigned DCD: 1;
+        unsigned DSR: 1;
+        unsigned RX: 1;
+        unsigned TX: 1;
+        unsigned RT: 1;
+        unsigned FE: 1;
+        unsigned PE: 1;
+        unsigned BE: 1;
+        unsigned OE: 1;
+        unsigned EOT: 1;
+        unsigned BIT9: 1;
+        unsigned : 3;
+        unsigned DMARX: 1;
+        unsigned DMATX: 1;
+    }) RIS;
+
+    // offset 0x040
+    // UART Masked Interrupt Status
+    REGMAP_32(, {
+        unsigned RI: 1;
+        unsigned CTS: 1;
+        unsigned DCD: 1;
+        unsigned DSR: 1;
+        unsigned RX: 1;
+        unsigned TX: 1;
+        unsigned RT: 1;
+        unsigned FE: 1;
+        unsigned PE: 1;
+        unsigned BE: 1;
+        unsigned OE: 1;
+        unsigned EOT: 1;
+        unsigned BIT9: 1;
+        unsigned : 3;
+        unsigned DMARX: 1;
+        unsigned DMATX: 1;
+    }) MIS;
+
+    // offset 0x044
+    // UARTInterrupt Clear
+    REGMAP_32(, {
+            unsigned RI: 1;
+            unsigned CTS: 1;
+            unsigned DCD: 1;
+            unsigned DSR: 1;
+            unsigned RX: 1;
+            unsigned TX: 1;
+            unsigned RT: 1;
+            unsigned FE: 1;
+            unsigned PE: 1;
+            unsigned BE: 1;
+            unsigned OE: 1;
+            unsigned EOT: 1;
+            unsigned BIT9: 1;
+            unsigned : 3;
+            unsigned DMARX: 1;
+            unsigned DMATX: 1;
+    }) ICR;
+
+    // offset 0x048
+    // UART DMA Control
+    REGMAP_32(, {
+        unsigned RXDMAE: 1;
+        unsigned TXDMAE: 1;
+        unsigned DMAERR: 1;
+    }) DMACTL;
+
+    char _reserved_02[0x058];
+
+    // offset 0x0A4
+    // UART 9-Bit Self Address
+    REGMAP_32(, {
+        unsigned ADDR: 8;
+        unsigned : 7;
+        unsigned BIT9EN: 1;
+    }) BIT9ADDR;
+
+    // offset 0x0A8
+    // UART 9-Bit Self Address Mask
+    REGMAP_32(, {
+        unsigned MASK: 8;
+    }) BIT9AMASK;
+
+    char _reserved_03[0xF14];
+
     // offset 0xFC0
     uint32_t PP;    // UART Peripheral Properties
-    char _reserved_01[4];
+
+    char _reserved_04[4];
 
     // offset 0xFC8
     // UART Clock Configuration
@@ -92,7 +232,8 @@ PAGE_MAP (UART_MAP, {
         unsigned CS: 4;
     }) CC;
 
-    char _reserved_02[4];
+    char _reserved_05[4];
+
     // offset 0xFD0
     uint32_t PeriphID4;
     uint32_t PeriphID5;
