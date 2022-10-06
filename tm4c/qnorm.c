e18e9ad2fc0cbbf72270457f7e1f5d7025cefde2
Robert J. Rouquette <robert.rouquette@focusvq.com>
2022-10-05 19:30:59 -0500

    update qnorm code

--- tm4c/qnorm.c
+++ tm4c/qnorm.c
@@ -7,91 +7,35 @@
 
 #include "qnorm.h"
 
-// neighbor distance lookup table
-static float lutZeta[QNORM_SIZE] = {
-        1.00000000e+00f,
-        1.83156389e-02f,
-        3.35462628e-04f,
-        6.14421235e-06f,
-        1.12535175e-07f,
-        2.06115362e-09f,
-        3.77513454e-11f,
-        6.91440011e-13f,
-        1.26641655e-14f,
-        2.31952283e-16f,
-        4.24835426e-18f,
-        7.78113224e-20f,
-        1.42516408e-21f,
-        2.61027907e-23f,
-        4.78089288e-25f,
-        8.75651076e-27f,
-        1.60381089e-28f,
-        2.93748211e-30f,
-        5.38018616e-32f,
-        9.85415469e-34f,
-        1.80485139e-35f,
-        3.30570063e-37f,
-        6.05460190e-39f,
-        1.10893902e-40f,
-        2.03109266e-42f,
-        3.72007598e-44f
-        // single precision drops out at index 26
-};
 
-
-static void updateBin(struct QNormBin *bin, float mu, float x, float w) {
+static inline void updateBin(struct QNormBin *bin, float mu, float x, float w) {
     bin->tau += w;
     bin->mean += (x - mu) * w;
 }
 
-static void compBounds(const struct QNorm *qnorm, float *_offset, float *_scale) {
-    // compute mean
-    float muY = 0;
-    for(int i = 0; i < QNORM_SIZE; i++) {
-        muY += qnorm->bins[i].mean * qnorm->bins[i].tau;
-    }
-
-    // compute covariance
-    float acc = 0, m = 0, n = 0;
-    for(int i = 0; i < QNORM_SIZE; i++) {
-        float x = acc + (0.5f * qnorm->bins[i].tau);
-        acc += qnorm->bins[i].tau;
-
-        float dx = x - 0.5f;
-        float dy = qnorm->bins[i].mean - muY;
-
-        m += dx * dy * qnorm->bins[i].tau;
-        n += dx * dx * qnorm->bins[i].tau;
-    }
-
-    // compute bounds
-    m /= n;
-    *_scale = 0.3f * m;
-    *_offset = muY;
-}
-
 static void compUpdate(struct QNorm *qnorm, float x, struct QNormBin *acc) {
-    int a = 1;
-    int b = QNORM_SIZE - 1;
     struct QNormBin *bins = qnorm->bins;
-
-    // locate nearest span
-    while(a < b) {
-        const int i = (a + b) / 2;
-        const float t = bins[i].mean;
-        if      (x < t) b = i;
-        else if (x > t) a = i+1;
-        else            break;
+    // locate pivot bin
+    int i = QNORM_SIZE / 2;
+    if(x > bins[i].mean) {
+        // ascending search
+        while (++i < QNORM_SIZE-1 && x > bins[i].mean);
+    } else {
+        // descending search
+        while (--i > 0 && x <= bins[i].mean);
+        ++i;
     }
-    int i = (a + b) / 2;
     // locate nearest bin
-    if(fabsf(x - bins[i].mean) > fabsf(x - bins[i-1].mean)) --i;
+    if((bins[i].mean - x) > (x - bins[i-1].mean)) --i;
 
-    // update bins
-    for(int j = 0; j < i; j++)
-        updateBin(acc + j, bins[j].mean, x, lutZeta[i-j]);
-    for(int j = i; j < QNORM_SIZE; j++)
-        updateBin(acc + j, bins[j].mean, x, lutZeta[j-i]);
+    // update nearest bin
+    updateBin(acc + i, bins[i].mean, x, 1.0f);
+    // update neighboring bins
+    if(i > 0           ) updateBin(acc + i-1, bins[i-1].mean, x, 0x1p-3f);
+    if(i < QNORM_SIZE-1) updateBin(acc + i+1, bins[i+1].mean, x, 0x1p-3f);
+    // update neighboring bins
+    if(i > 1           ) updateBin(acc + i-2, bins[i-2].mean, x, 0x1p-10f);
+    if(i < QNORM_SIZE-2) updateBin(acc + i+2, bins[i+2].mean, x, 0x1p-10f);
 }
 
 static void sort(float *begin, float *end);
@@ -157,50 +101,64 @@ int qnorm_update(struct QNorm *qnorm, const float *data, int count, int stride, 
         data += stride;
     }
 
-    // compute eta
-    const float eta = (qnorm->tau > 0) ? (1.0f / qnorm->tau) : 1.0f;
-    for(int i = 0; i < QNORM_SIZE; i++) {
-        if(acc[i].tau <= 0) continue;
-        qnorm->bins[i].tau += eta * acc[i].tau;
-        qnorm->bins[i].mean += eta * acc[i].mean / qnorm->bins[i].tau;
-    }
-
     // update global tau
-    qnorm->tau += (float) count;
-    if(qnorm->tau > QNORM_LIM) qnorm->tau = QNORM_LIM;
-
-    // normalize bin taus
-    float nrm = 0;
-    for(int i = 0; i < QNORM_SIZE; i++)
-        nrm += qnorm->bins[i].tau;
-    nrm = 1.0f / nrm;
-    for(int i = 0; i < QNORM_SIZE; i++)
-        qnorm->bins[i].tau *= nrm;
-
-    // compute new bounds
-    float _offset, _rscale;
-    compBounds(qnorm, &_offset, &_rscale);
-    float denom = qnorm->rscale + _rscale;
-
-    // only update if there has been a significant change
-    if(
-        fabsf(_offset - qnorm->offset) / denom > 0.01f ||
-        fabsf(_rscale - qnorm->rscale) / denom > 0.01f
-    ) {
-        // compute scale and offset translation from old bounds to new bounds
-        *dScale =  qnorm->rscale / _rscale;
-        *dOffset = (qnorm->offset - _offset) / _rscale;
-        // set new scale
-        qnorm->offset = _offset;
-        qnorm->tscale = 1.0f / _rscale;
-        qnorm->rscale = _rscale;
-        return 1;
+    if(qnorm->tau < QNORM_LIM) {
+        qnorm->tau += (float) count;
+        if (qnorm->tau > QNORM_LIM)
+            qnorm->tau = QNORM_LIM;
     }
 
-    // no change to scaling transform
-    *dScale = 1;
-    *dOffset = 0;
-    return 0;
+    // compute eta
+    const float eta = 1.0f / qnorm->tau;
+    float nrm = 0;
+    for(int i = 0; i < QNORM_SIZE; i++) {
+        struct QNormBin *bin = qnorm->bins + i;
+        if(acc[i].tau > 0) {
+            bin->tau += eta * acc[i].tau;
+            bin->mean += eta * acc[i].mean / bin->tau;
+        }
+        nrm += bin->tau;
+    }
+
+    // only update scaling if sufficient entropy has accumulated
+    if(nrm < 1.05f) {
+        // no change to scaling transform
+        *dScale = 1;
+        *dOffset = 0;
+        return 0;
+    }
+
+    // normalize bin taus and compute new offset
+    nrm = 1.0f / nrm;
+    float offset = 0;
+    for(int i = 0; i < QNORM_SIZE; i++) {
+        struct QNormBin *bin = qnorm->bins + i;
+        bin->tau *= nrm;
+        offset += bin->mean * bin->tau;
+    }
+
+    // compute correlation
+    float z = 0, m = 0, n = 0;
+    for(int i = 0; i < QNORM_SIZE; i++) {
+        struct QNormBin *bin = qnorm->bins + i;
+
+        float dx = z + (0.5f * bin->tau) - 0.5f;
+        float dy = bin->mean - offset;
+
+        m += bin->tau * dx * dy;
+        n += bin->tau * dx * dx;
+        z += bin->tau;
+    }
+
+    // compute scale and offset translation from old bounds to new bounds
+    float rscale = 0.3f * m / n;
+    *dScale  =  qnorm->rscale / rscale;
+    *dOffset = (qnorm->offset - offset) / rscale;
+    // set new scale
+    qnorm->offset = offset;
+    qnorm->tscale = 1.0f / rscale;
+    qnorm->rscale = rscale;
+    return 1;
 }
 
 float qnorm_transform(const struct QNorm *qnorm, float x) {
