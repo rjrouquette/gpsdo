10ded76383501f03b2b38ea4d4737b6afe57d4ef
Robert J. Rouquette <robert.rouquette@focusvq.com>
2023-05-03 21:07:09 -0500

    update supported MIBs in influxdb.py

--- influx.py
+++ influx.py
@@ -29,17 +29,16 @@ while True:
                                 errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
 
         proc_temp = int(varBinds[0][1]) / 1000
-        dcxo_temp = int(varBinds[1][1]) / 1000
-        err_mean = int(varBinds[2][1]) / 10
-        err_rms = int(varBinds[3][1]) / 10
-        pll_skew = int(varBinds[4][1]) / 10000
-        pll_ppm = int(varBinds[5][1]) / 10000
-        comp_bias = int(varBinds[6][1]) / 10
-        comp_coef = int(varBinds[7][1]) / 10
-        comp_val = int(varBinds[8][1]) / 10
+        err_mean = int(varBinds[1][1]) / 10
+        err_rms = int(varBinds[2][1]) / 10
+        pll_skew = int(varBinds[3][1]) / 10000
+        pll_ppm = int(varBinds[4][1]) / 10000
+        comp_bias = int(varBinds[5][1]) / 10
+        comp_coef = int(varBinds[6][1]) / 10
+        comp_val = int(varBinds[7][1]) / 10
 
-        body = 'gpsdo,host=%s proc_temp=%.2f,dcxo_temp=%.2f,pll_error=%.1f,pll_rmse=%.1f,freq_skew=%.4f,pll_ppm=%.4f,comp_m=%.1f,comp_b=%.1f,comp_v=%.1f' % (
-            gpsdoHost, proc_temp, dcxo_temp, err_mean, err_rms, pll_skew, pll_ppm, comp_coef, comp_bias, comp_val
+        body = 'gpsdo,host=%s proc_temp=%.3f,pll_error=%.1f,pll_rmse=%.1f,freq_skew=%.4f,pll_ppm=%.4f,comp_m=%.1f,comp_b=%.1f,comp_v=%.1f' % (
+            gpsdoHost, proc_temp, err_mean, err_rms, pll_skew, pll_ppm, comp_coef, comp_bias, comp_val
         )
         requests.post(influxUrl, data=body, timeout=1)
     except:
