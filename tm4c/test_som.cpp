0867c47fb570a8e81fb4c237ab23344c0f01dcb3
Robert J. Rouquette <robert.rouquette@focusvq.com>
2023-09-18 13:25:46 -0500

    evaluate new compensation regression method

--- tm4c/test_som.cpp
+++ tm4c/test_som.cpp
@@ -9,23 +9,63 @@
 #include <cstring>
 
 float som[16][3] = {
-        35.4101632, -176.783888, 0.999986816,
-        36.5221952, -175.763024, 0.999984256,
-        36.8832832, -175.241488, 0.99997312,
-        37.0535616, -175.1744, 0.99997312,
-        37.4759648, -174.996112, 0.99997312,
-        37.9842272, -174.803344, 0.99997312,
-        38.5020384, -174.706112, 0.99997312,
-        38.9364288, -174.572928, 0.99997312,
-        39.5659328, -174.247584, 0.999973248,
-        40.3481888, -173.858384, 0.999973312,
-        41.0989376, -173.529728, 0.999973568,
-        41.7298912, -173.375584, 0.9999856,
-        42.4995968, -173.029376, 0.999996544,
-        42.9004512, -172.737168, 0.999997696,
-        43.4969888, -172.517664, 0.999998592,
-        44.2094272, -172.198032, 0.999999232
+        38.1113888, -172.392272, 0.999986816,
+        38.60752, -172.171296, 0.999984256,
+        38.8272064, -172.07312, 0.99997312,
+        38.9498336, -172.012256, 0.99997312,
+        39.04688, -171.97296, 0.99997312,
+        39.2285312, -171.900272, 0.99997312,
+        39.475696, -171.796816, 0.99997312,
+        39.905552, -171.608272, 0.99997312,
+        40.3379456, -171.416832, 0.999973248,
+        40.8939616, -171.174208, 0.999973312,
+        41.4561856, -170.94584, 0.999973568,
+        41.959504, -170.742576, 0.9999856,
+        42.6209728, -170.468496, 0.999996544,
+        43.763024, -170.015536, 0.999997696,
+        44.6834688, -169.66192, 0.999998592,
+        45.9950176, -169.162288, 0.999999232
 };
+
+void fitGradient(const float * const data, const int cnt, const int dim, float alpha, float *coef) {
+    float delta[dim+1];
+    for(int j = 0; j <= dim; j++)
+        delta[j] = 0;
+
+    for(int i = 0; i < cnt; i++) {
+        auto row = data + (i * 3);
+        float error = row[1];
+        float x = 1;
+        for(int j = 0; j <= dim; j++) {
+            error -= x * coef[j];
+            x *= row[0];
+        }
+
+        error *= row[2];
+        x = 1;
+        for(int j = 0; j <= dim; j++) {
+            delta[j] += error * x;
+            x *= row[0];
+        }
+    }
+
+    alpha /= cnt;
+    for(int j = 0; j <= dim; j++)
+        coef[j] += alpha * delta[j];
+}
+
+void fitLinear(const float * const data, const int cnt, float *coef) {
+    float xx = 0, xy = 0;
+    for(int i = 0; i < cnt; i++) {
+        auto row = data + (i * 3);
+        float x = row[0];
+        float y = row[1];
+
+        xx += x * x * row[2];
+        xy += x * y * row[2];
+    }
+    coef[0] = xy / xx;
+}
 
 void fitLinear(const float * const data, const int cnt, float *coef, float *mean) {
     // compute means
@@ -84,7 +124,67 @@ void fitTaylor(const int order, float * const data, const int cnt, float *coef, 
     }
 }
 
+int main(int argc, char **argv) {
+    float scratch[sizeof(som) / sizeof(float)];
+    memcpy(scratch, som, sizeof(som));
 
+    // re-center data
+    float mean[3];
+    bzero(mean, sizeof(mean));
+
+    // compute means
+    for(int i = 0; i < 16; i++) {
+        auto row = scratch + (i * 3);
+        mean[0] += row[0] * row[2];
+        mean[1] += row[1] * row[2];
+        mean[2] += row[2];
+    }
+    mean[0] /= mean[2];
+    mean[1] /= mean[2];
+
+    // re-center data
+    for(int i = 0; i < 16; i++) {
+        auto row = scratch + (i * 3);
+        row[0] -= mean[0];
+        row[1] -= mean[1];
+    }
+
+    float coef[4];
+    bzero(coef, sizeof(coef));
+    fitLinear(scratch, 16, coef + 1);
+    for (int i = 0; i < 1<<16; i++)
+        fitGradient(scratch, 16, 2, 0x1p-12f, coef);
+
+    for (auto c : coef)
+        fprintf(stdout, "%f\n", c);
+
+    fprintf(stdout, "plot:\n");
+    fprintf(stdout, "temp,som,linear,quadratic,cubic,error\n");
+    float rmse = 0;
+    for(auto &row : som) {
+        fprintf(stdout, "%f,%f", row[0], row[1]);
+        float x = row[0] - mean[0];
+        float y = coef[0] + mean[1];
+        y += x * coef[1];
+        fprintf(stdout, ",%f", y);
+        y += x * x * coef[2];
+        fprintf(stdout, ",%f", y);
+        y += x * x * x * coef[3];
+        fprintf(stdout, ",%f", y);
+        y -= row[1];
+        fprintf(stdout, ",%f\n", y);
+        rmse += y * y * row[2];
+    }
+    fflush(stdout);
+
+    rmse = sqrtf(rmse / mean[2]);
+    fprintf(stdout, "\nrmse: %f\n", rmse);
+    fflush(stdout);
+
+    return 0;
+}
+
+/*
 int main(int argc, char **argv) {
     float scratch[sizeof(som) / sizeof(float)];
     memcpy(scratch, som, sizeof(som));
@@ -101,14 +201,14 @@ int main(int argc, char **argv) {
         y -= row[1];
         rmse += y * y * row[2];
     }
-    rmse = sqrtf(rmse / mean[2]);
+    rmse /= mean[2];
+
     for(auto &row : som) {
         float x = row[0] - mean[0];
         float y = coef[0] + mean[1];
         y += x * coef[1];
         y -= row[1];
-        if(fabsf(y) > rmse * 2)
-            row[2] = 0;
+        row[2] *= expf(-0.25 * (y * y) / rmse);
     }
 
     memcpy(scratch, som, sizeof(som));
@@ -144,3 +244,4 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+*/
